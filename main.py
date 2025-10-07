ff92851c6a8d2ee66c5aef83eafa6c33b17d3638
dukeoftoucan <130246192+yuvan4@users.noreply.github.com>
2025-10-07 22:16:46 +0000

    add api to dotenv

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-# step 1: import requests
+import requests # import requests library for use
+from dotenv import dotenv_values
+key = dotenv_values(".env")["SEATS_AERO_API_KEY"]
 
-# step 2: call api with given endpoint and key
+api_result = requests.get(f'https://api.aviationstack.com/v1/flights?access_key={key}') # requests the data from the endpoint
+# to add other parameters you can add "&parameter=value&parameter2=value2" etc.
 
-# step 3: convert to dictionary and grab only the "data" value
+# Or you can do it this way
+# payload = {
+    # "access_key" : "2c60a151dd8c99eb71b9547bfeaac43a"
+# }
+# api_result = requests.get('https://api.aviationstack.com/v1/flights', params=payload)
 
-# step 4: loop through the flights and check which flights are active
+api_result = api_result.json() # converts json to dicitonary
+data = api_result["data"] # grabs only the "data" element from the response
 
-# step 5: if active, print the airline, date, departure airport and arrival airport
+
+for flight in data: # loops through all flights 
+    if (flight["flight_status"] == "active"): # checks if flight is active 
+        print("Flight airline: ", flight["airline"]["name"]) # airline
+        print("Flight date: " + flight["flight_date"]) # date of flight
+        print("Flight departure airport: " + flight["departure"]["airport"]) # departure airport
+        print("Flight arrival airport: " + flight["arrival"]["airport"]) # arrival airport
+        print("-----------------------------------------------")
+        
